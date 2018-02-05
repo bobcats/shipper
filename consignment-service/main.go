a4f5359f3cbbcf10ba7881668a6249351c1cf929
bobcats <brian@madsalty.com>
2018-02-04 23:31:21 -0800

    Build vessel service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	pb "github.com/bobcats/shipper/consignment-service/proto/consignment"
+	vesselProto "github.com/bobcats/shipper/vessel-service/proto/vessel"
 	micro "github.com/micro/go-micro"
 	"golang.org/x/net/context"
 )
@@ -31,13 +32,24 @@ func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 }
 
 type service struct {
-	repo Repository
+	repo         Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+	req.VesselId = vesselResponse.Vessel.Id
+
 	consignment, err := s.repo.Create(req)
 
 	if err != nil {
@@ -63,9 +75,11 @@ func main() {
 		micro.Version("latest"),
 	)
 
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
