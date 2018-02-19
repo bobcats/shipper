0fe4051eb4cab1b2b9a1e716f1c2540a183e4da4
bobcats <brian@madsalty.com>
2018-02-18 23:22:01 -0800

    Push new proto files

---
+++ user-service/repository.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	pb "github.com/bobcats/shipper/user-service/proto/user"
+	"github.com/jinzhu/gorm"
+)
+
+type Repository interface {
+	GetAll() ([]*pb.User, error)
+	Get(id string) (*pb.User, error)
+	Create(user *pb.User) error
+	GetByEmailAndPassword(user *pb.User) (*pb.User, error)
+}
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func (repo *UserRepository) GetAll() ([]*pb.User, error)
