edbf9c1802569459d83aa54d74bf2c0ff55119e1
bobcats <brian@madsalty.com>
2018-02-19 00:01:11 -0800

    Swap to google uuid because satori recently had breaking change without tag

--- user-service/proto/user/extensions.go
+++ user-service/proto/user/extensions.go
@@ -1,14 +1,11 @@
 package go_micro_srv_user
 
 import (
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid, err := uuid.NewV4()
-	if err != nil {
-		return err
-	}
+	uuid := uuid.New()
 	return scope.SetColumn("Id", uuid.String())
 }
